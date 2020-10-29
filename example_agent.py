332b73969a5535e737e53c9de374ee00f87a8cf2
Apoorv Khandelwal <apoorv.khand@gmail.com>
2020-10-28 20:22:13 -0700

    Create example_agent.py

---
+++ example_agent.py
@@ -0,0 +1,24 @@
+from ai2thor_docker.x_server import startx
+from ai2thor_orbslam2 import ORBSLAM2Controller
+import random
+
+
+if __name__ == '__main__':
+    startx()
+    controller = ORBSLAM2Controller(scene='FloorPlan28', gridSize=0.0$
+    for _ in range(1000):
+        rval = random.randint(0, 5)
+        if rval == 0:
+            controller.step(action='MoveAhead')
+        elif rval == 1:
+            controller.step(action='MoveBack')
+        elif rval == 2:
+            controller.step(action='MoveRight')
+        elif rval == 3:
+            controller.step(action='MoveLeft')
+        elif rval == 4:
+            controller.step(action='RotateRight', degrees=2.0)
+        elif rval == 5:
+            controller.step(action='RotateLeft', degrees=2.0)
+    controller.stop()
+    

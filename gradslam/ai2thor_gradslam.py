f4de0ca630edb1144fa49e75811a3114e13d5290
apoorvkh <apoorv.khand@gmail.com>
2020-11-17 05:39:24 -0800

    Added agent and updated controller for gradslam

--- gradslam/ai2thor_gradslam.py
+++ gradslam/ai2thor_gradslam.py
@@ -1,47 +1,70 @@
 import os
+import math
 from time import time
 from ai2thor.controller import Controller
-import orbslam2
-import matplotlib.pyplot as plt
+from gradslam import Pointclouds, RGBDImages
+from gradslam.slam import PointFusion
+import numpy as np
+import torch
 
 
-class ORBSLAM2Controller(Controller):
-    def __init__(self, vocab_file, settings_file, *args, **kwargs):
+def get_intrinsics(event):
+    fov = event.metadata['fov']
+    w, h = event.metadata['screenWidth'], event.metadata['screenHeight']
+    intrinsics = np.eye(4)
+    intrinsics[0, 0] = w / (2 * math.tan(math.radians(fov / 2)))  # f_x
+    intrinsics[1, 1] = h / (2 * math.tan(math.radians(fov / 2)))  # f_y
+    intrinsics[0, 2] = w / 2  # c_x
+    intrinsics[1, 2] = h / 2  # c_y
+    return intrinsics
+
+def to_rgbd(frame, depth_frame, intrinsics, poses, device=None):
+    frame = torch.tensor([[frame]], dtype=torch.float32, device=device)
+    depth_frame = torch.tensor([[depth_frame]], dtype=torch.float32, device=device).unsqueeze(-1)
+    intrinsics = torch.tensor([[intrinsics]], dtype=torch.float32, device=device)
+    rgbd_images = RGBDImages(frame, depth_frame, intrinsics, poses, device=device)
+    return rgbd_images
+
+class GradslamController(Controller):
+    def __init__(self, device, *args, **kwargs):
+        self.super_init = False
         super().__init__(*args, **kwargs)
-        self.slam_system = orbslam2.SLAM(vocab_file, settings_file, 'monocular')
+        self.super_init = True
+        #
+        self.device = device
+        self.slam = PointFusion(device=device)
+        self.pointclouds = Pointclouds(device=device)
+        self.live_frame, self.prev_frame = None, None
+        #
         self.frame_counter = 0
         self.time_elapsed = 0.0
-        self.gt_trajectory = []
 
     def step(self, *args, **kwargs):
-        start = time()
+        if self.super_init:
+            start = time()
+
         event = super().step(*args, **kwargs)
-        if hasattr(self, 'slam_system'):
-            pose = self.slam_system.track(event.frame)
+
+        if self.super_init:
+            if not hasattr(self, 'intrinsics'):
+                self.intrinsics = get_intrinsics(event)
+            if self.prev_frame is None:
+                poses = torch.eye(4, device=self.device).view(1, 1, 4, 4)
+            else:
+                poses = self.prev_frame.poses
+            self.live_frame = to_rgbd(event.frame, event.depth_frame, self.intrinsics, poses, device=self.device)
+            self.pointclouds, self.live_frame.poses = self.slam.step(self.pointclouds, self.live_frame, self.prev_frame)
+            self.prev_frame = self.live_frame
+            #
             self.frame_counter += 1
             self.time_elapsed += time() - start
-        if 'action' in kwargs and kwargs['action'].startswith('Move'):
-            self.gt_trajectory.append((event.metadata['agent']['position']['x'], event.metadata['agent']['position']['z']))
+
         return event
 
-    def stop(self, keyframe_traj, traj_plot, *args, **kwargs):
+    def stop(self, *args, **kwargs):
         super().stop(*args, **kwargs)
-        self.slam_system.shutdown()
-        self.slam_system.save_keyframe_trajectory(keyframe_traj)
-
-        with open(keyframe_traj, 'r') as rf:
-            keyframes = rf.readlines()
-            keyframes = list(zip(*[kf.split()[1:4] for kf in keyframes]))
-            kf_x = list(map(float, keyframes[0]))
-            kf_z = list(map(float, keyframes[2]))
-
-        plt.scatter(kf_x, kf_z)
-        plt.savefig(traj_plot)
-        plt.clf()
-
-        gt_camera_x, gt_camera_z = list(zip(*self.gt_trajectory))
-        plt.scatter(gt_camera_x, gt_camera_z)
-        plt.savefig('gt_traj.png')
+        with open('pointclouds.html', 'w') as out:
+            out.write(self.pointclouds.plotly(0).to_html())
 
     def fps(self):
         return self.frame_counter / self.time_elapsed
---
+++ gradslam/demo/example_agent.py
@@ -0,0 +1,115 @@
+import random
+import math
+import torch
+
+import dijkstra
+from ai2thor_docker.x_server import startx
+from ai2thor_gradslam import GradslamController
+
+
+def dist(src, dst):
+    del_x, del_z = (dst['x'] - src['x'], dst['z'] - src['z'])
+    return math.sqrt(math.pow(del_x, 2) + math.pow(del_z, 2))
+
+def get_rot(src, dst):
+    del_x, del_z = (dst['x'] - src['x'], dst['z'] - src['z'])
+    if del_z > 0:
+        if del_x == 0:
+            theta = math.pi / 2
+        else:
+            theta = math.atan(del_z / del_x)
+            if del_x < 0:
+                theta = math.pi - theta
+    elif del_z < 0:
+        if del_x == 0:
+            theta = 3/2 * math.pi
+        else:
+            theta = math.atan(del_x / del_z)
+            theta = (2 if del_x > 0 else 3/2) * math.pi - theta
+    elif del_x < 0:
+        theta = math.pi
+    else:
+        theta = 0.0
+    return math.degrees(theta)
+
+def goto(src, src_rot, dst):
+    rot_to_dst = src_rot - get_rot(src, dst)
+    rotate_action = 'RotateRight' if rot_to_dst >= 0 else 'RotateLeft'
+    rot_to_dst = abs(rot_to_dst)
+    while rot_to_dst > 0:
+        del_r = min(rot_to_dst, rotateStepDegrees)
+        event = controller.step(action=rotate_action, degrees=del_r)
+        rot_to_dst -= del_r
+    dist_to_dst = dist(src, dst)
+    while dist_to_dst > 0:
+        d_m = min(dist_to_dst, gridSize)
+        event = controller.step(action='MoveAhead', moveMagnitude=d_m)
+        dist_to_dst -= d_m
+    return event
+
+def follow_path(src, src_rot, path):
+    for p in path:
+        event = goto(src, src_rot, p)
+        src = event.metadata['agent']['position']
+        src_rot = event.metadata['agent']['rotation']['y']
+    return event
+
+def random_walk(controller):
+    event = controller.step(action='GetReachablePositions')
+    reachable_positions = event.metadata['actionReturn']
+    src = event.metadata['agent']['position']
+    src_rot = event.metadata['agent']['rotation']['y']
+
+    src_index = reachable_positions.index(src)
+    src_dists = [dist(src, dst) for dst in reachable_positions]
+
+    graph = {}
+    for i, p1 in enumerate(reachable_positions):
+        dists = {}
+        for j, p2 in enumerate(reachable_positions):
+            d = dist(p1, p2)
+            if d <= gridSize and i != j:
+                dists[j] = d
+        graph[i] = dists
+
+    path = None
+    while path is None:
+        dst = random.choice(reachable_positions)
+        dst_index = reachable_positions.index(dst)
+        if dist(src, dst) > 0.5 * max(src_dists):
+            path = dijkstra.shortest_path(graph, src_index, dst_index)
+    path = [reachable_positions[i] for i in path]
+
+    follow_path(src, src_rot, path)
+
+def random_shortest_path(controller):
+    event = controller.step(action='GetReachablePositions')
+    reachable_positions = event.metadata['actionReturn']
+    src = event.metadata['agent']['position']
+    src_rot = event.metadata['agent']['rotation']['y']
+
+    src_index = reachable_positions.index(src)
+    src_dists = [dist(src, dst) for dst in reachable_positions]
+
+    dst_index = src_index
+    while src_dists[dst_index] < 0.5 * max(src_dists):
+        dst_index = random.choice(range(len(reachable_positions)))
+    dst = reachable_positions[dst_index]
+
+    path = controller.step(action="GetShortestPathToPoint", position=src, x=dst['x'], y=dst['y'], z=dst['z']).metadata['actionReturn']['corners']
+
+    follow_path(src, src_rot, path[: int(0.5 * len(path)) ])
+
+
+if __name__ == '__main__':
+    startx()
+
+    device = torch.device('cuda')
+    gridSize = 0.05
+    rotateStepDegrees = 2.0
+    controller = GradslamController(device, scene='FloorPlan28', gridSize=gridSize, snapToGrid=False, renderDepthImage=True)
+
+    random_shortest_path(controller)
+
+    controller.stop()
+    print('FPS: %.2f' % controller.fps())
